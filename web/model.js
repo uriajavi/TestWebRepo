5221b4b082bb582f58a5a390918ab5b906d549ba
javi <javi@LAPTOP-WIN10JJ>
2025-10-22 14:10:33 +0200

    Comments changing and Customer class definition.

---
+++ web/model.js
@@ -0,0 +1,29 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function Customer(  id, 
+                    firstName, 
+                    lastName, 
+                    middleInitial, 
+                    street, 
+                    city, 
+                    state,
+                    zip,
+                    phone,
+                    email,
+                    password) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.middleInitial = middleInitial;
+    this.street = street;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+    this.phone = phone;
+    this.email = email;
+    this.password = password;
+}
+
