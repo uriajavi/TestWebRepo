16994ae31797d1f5c6ef418d87f60a18653659b9
uriajavi <uriajavi@gmail.com>
2026-01-30 19:51:33 +0100

    Add TODO comments for Customer function enhancements
    Added TODO comments for future improvements to the Customer function.

--- web/model.js
+++ web/model.js
@@ -1,7 +1,6 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * @todo Convert function to Class Customer
+ * @todo Add toJSON method in order for Customer to be formatted as server side expects when stringifying 
  */
 function Customer(  id, 
                     firstName, 
